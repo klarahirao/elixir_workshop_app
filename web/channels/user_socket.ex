5e3a12345b7413f3563cde7a2b13f10955d90b7d
Klara <klara.hirao@elpassion.pl>
2016-09-04 10:58:35 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ElixirWorkshopApp.UserSocket do
   channel "room:*", ElixirWorkshopApp.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
